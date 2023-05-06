15b7df2f9cd4b5f15362af68d43f432c8a10cf2c
Halvor <h.mangseth@gmail.com>
2023-05-06 14:20:44 +0200

    del 3

---
+++ del_3/fasit-mappa/halvbistro.py
@@ -0,0 +1,11 @@
+def load_inventory(event, context):
+  bucket = event['Records'][0]['s3']['bucket']['name']
+  file= event['Records'][0]['s3']['object']['key']
+  print("bucket: " + bucket + ", file: " + file + " received")
+  return {
+      "statusCode": 200,
+      "body": "success",
+  }
+
+if __name__ == '__main__':
+  load_inventory()
